40afc5716166264b231e84f5afe750028cd2cfed
Alexander Yakubovskiy <bloomzakk@gmail.com>
2019-02-13 20:21:37 +0200

    Fetch update

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-$('.card').ajax({
-  url: 'https://randomuser.me/api/',
-  dataType: 'json',
-  success: function(data) {
-    console.log(data);
-  }
-});
+const img = document.getElementById("imag")
+let object;
+
+fetch('https://randomuser.me/api/')
+                                .then(function(response){
+                                  if(response.ok){
+                                    return response.json();
+                                  }
+                                })
+                                .then(function(data) {
+                                  let people = data.results;
+                                  imag.src = people[0].picture.large;
+                                });
