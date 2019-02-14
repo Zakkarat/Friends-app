6039a1dd68b6c1095d171edbf98b4171429eb6ad
Alexander Yakubovskiy <bloomzakk@gmail.com>
2019-02-14 16:53:09 +0200

    Color changes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const img = document.getElementById("imag");
 const main = document.getElementsByTagName("main")[0];
-let object;
+let people;
 
 fetch('https://randomuser.me/api/?results=30')
                                 .then(function(response){
@@ -13,13 +13,16 @@ fetch('https://randomuser.me/api/?results=30')
                                 });
 
 function AddCard(data) {
-  let people = data.results;
+  people = data.results;
+  people = noMuslim(people);
   people.forEach(function(info) {
     CapitalNames(info);
+    info.location.city = CapitalFirst(info.location.city);
     main.innerHTML += `<div class="card">
       <img src="${info.picture.large}" id="imag" />
       <h1>${info.name.first} ${info.name.last}</h1>
       <h3>Age: ${info.dob.age}</h3>
+      <h3>Location: ${info.location.city}</h3>
       <h4>Email: ${info.email}</h4>
       <h3>Phone: ${info.phone}</h3>
     </div>`
@@ -33,4 +36,10 @@ function CapitalFirst(name) {
 function CapitalNames(info) {
   info.name.first = CapitalFirst(info.name.first);
   info.name.last = CapitalFirst(info.name.last);
+}
+function noMuslim(people) {
+    let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
+"q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    people = people.filter(info => info.name.first.charAt(0) != alphabet.map(letter => letter));
+    return people;
 }
